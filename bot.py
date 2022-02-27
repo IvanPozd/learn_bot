0ec78e1e4bd21c4f1654a5ea9e846d9616cb1791
IvanPozd <johndillon28cb@gmail.com>
2022-02-27 21:11:26 +0300

    add proxy

--- bot.py
+++ bot.py
@@ -2,21 +2,30 @@
 """ Simple Echo Bot """
 
 import logging
+import settings
 
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from config import TOKEN
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, Dispatcher
 
+PROXY = {
+	'proxy_url': settings.PROXY_URL,
+	'urllib3_proxy_kwargs': {
+		'username': settings.PROXY_USERNAME,
+		'password': settings.PROXY_PASSWORD
+	}
+}
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(filename="logs/bot.log",
+                    level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start_command(update: Update, context: CallbackContext) -> None:
+	logger.info(f'Starting bot. {update.effective_user}')
 	user = update.effective_user
 	update.message.reply_markdown_v2(
-		fr"Hi {user.mention_markdown_v2()}\!",
+		fr"Hi, {user.username}!",
 		reply_markup=ForceReply(selective=True)
 	)
 
@@ -30,12 +39,12 @@ def echo_command(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
-	updater = Updater(TOKEN)
-	dispatcher = updater.dispatcher
+	updater = Updater(settings.TOKEN, use_context=True, request_kwargs=PROXY)
+	dp = updater.dispatcher
 
-	dispatcher.add_handler(CommandHandler("start", start))
-	dispatcher.add_handler(CommandHandler("help", help_command))
-	dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_command))
+	dp.add_handler(CommandHandler("start", start_command))
+	dp.add_handler(CommandHandler("help", help_command))
+	dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_command))
 
 	updater.start_polling()
 	updater.idle()
